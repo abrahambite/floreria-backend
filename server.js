8bc7cff5a85913faf35917534919107b31f8b44b
abrahambite <abrahamercadologo@gmail.com>
2025-05-03 16:38:20 -0700

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const fileUpload = require('express-fileupload');
+const path = require('path');
+
+// Rutas
 const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
 
 dotenv.config();
 const app = express();
@@ -12,8 +16,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Endpoints
 app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -22,7 +29,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 }).then(() => console.log('✅ MongoDB conectado'))
   .catch(err => console.error('❌ Error al conectar MongoDB:', err));
 
-// Puerto dinámico para Render
+// Puerto
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
